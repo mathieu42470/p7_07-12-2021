4cf51ce6a33c2e90e7a86045969f07f7a7727aed
tartarin <m.tartarin@hotmail.fr>
2022-01-04 16:13:34 +0100

    changement html

--- backend/controllers/comment.js
+++ backend/controllers/comment.js
@@ -2,63 +2,57 @@ const Comment = require('../models/comment');
 const fs = require('fs');
 const { json } = require('express');
 
+const text = req.body.text;
+const url_image = req.body.url_image;
+const date = req.body.date;
+const id_commentaire = req.body._id;
 
 // création de la possibilité de commentaires //
-exports.createComment = (req, res, next) =>{
-               Message.findOne({_id: req.params.id})
-               const commentObjet = json.parse(req.body.comment);
-               delete commentObjet._id;
-               const comment = new Comment({
-                              // ...commentObjet,
-                              // likes: 0,
-                              // imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-               });
-               Comment.save()
-               .then(() => {               
-                db.query("Select * From commentaire",(error,resultat) =>{
+exports.createComment = (req, res, next) =>{                           
+                db.query(INSERT * INTO `commentaire` (`text`)
+                ({text: text})
+                )
+                Comment.save()
+                .then(() => {
                   if(error){
                     return res.status(400).json('error')
                   }
                   return res.status(200).json({message : resultat})
                 }) 
-          })
 };
 
 // récupération de tous les commentaires //
 exports.getAllComment = (req, res, next)=>{
-             
-               db.query("Select * From commentaire",(error,resultat) =>{
+               db.query(SELECT 
+                `commentaire`.text,
+            FROM `groupomania`.commentaire)
                 if(error){
                   return res.status(400).json('error')
                 }
                 return res.status(200).json({message : resultat})
-              }) 
 };
 
 // récupération d'un commentaire en particulier //
 exports.getOneComment = (req, res, next) =>{
-               Comment.findOne({_id: req.params.id})
-               db.query("Select * From commentaire WHERE comment_id = ",(error,resultat) =>{
+  db.query(SELECT `commentaire`.id_commentaire,
+  `commentaire`.text,
+  `commentaire`.url_image,
+  `commentaire`.date,
+  FROM `groupomania`.commentaire)
                 if(error){
                   return res.status(400).json('error')
                 }
                 return res.status(200).json({message : resultat})
-              }) 
 };
 
 // modifier un commentaire //
 exports.modifyComment = (req, res, next)=>{
-      const commentObjet = req.file?{
-                     ...json.parse(req.body.Comment),
-                     imageUrl:`${req.protocol}://${req.get('host')}/images/${req.file.filename}`    
-      } : { ...req.body };
-      Comment.updateOne({_id: req.params.id}, { ...req.body, _id: req.params.id})
-      db.query("Select * From commentaire",(error,resultat) =>{
+  db.query(UPDATE `groupomania`.commentaire ({text: text}, {url_image: url_image })
+  );
         if(error){
           return res.status(400).json('error')
         }
         return res.status(200).json({message : resultat})
-      }) 
 };
 
  // suppression d'un commentaire //
@@ -101,10 +95,11 @@ exports.likeComment = (req, res, next) =>{
     }
     Comment.save();
   })
-  db.query("Select * From commentaire",(error,resultat) =>{
+  db.query(INSERT * INTO `groupomania`.commentaire,
+  ({likes})
+  )
     if(error){
       return res.status(400).json('error')
     }
-    return res.status(200).json({message : resultat})
-  }) 
+    return res.status(200).json({message : resultat}) 
 };
