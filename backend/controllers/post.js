f7eaf7096770fe17396651af63a0a4c5d798e4ed
tartarin <m.tartarin@hotmail.fr>
2022-02-02 16:16:10 +0100

    changement like

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -53,7 +53,6 @@ exports.getOnepost = (req, res, next) =>{
 
 // modification d'un message //
 exports.modifyPost = (req, res, next) =>{
-  //const tab : [] = (req.body)
   db.query(`UPDATE post SET text = ? WHERE id_post= ? AND id_utilisateur= ?`, [req.body.text,req.body.id_post,req.body.id_utilisateur],(err, result) =>{
     if(err){
       return res.status(400).json({message : err.message})
@@ -76,16 +75,6 @@ exports.deletePost = (req, res, next) => {
 
 // like du message //
 exports.likePost = (req, res, next) =>{
-
-  // Quand on aime ou pas un post 
-    
-    // Faire une rqt SELECT avec un WHERE sur l'id du post pour récupérer la liste des utilisateur qui aiment et qui n'aiment pas.
-  // db.query(`SELECT * FROM groupomania.like WHERE id_user = ? AND id_post= ?`,[req.body.id_user, req.body.id_post], (err, result) =>{
-  //   if(err){
-  //     return res.status(400).json({message : err.message})
-  //   }else{
-    //ici construire un objet post avec le resultat de la rqt
-    console.log(req.body)
   
     switch(req.body.like){
       // message non like
@@ -99,67 +88,50 @@ exports.likePost = (req, res, next) =>{
             db.query(`DELETE FROM groupomania.like WHERE id_user= ? AND id_post= ?`,[req.body.id_user, req.body.id_post], (err, result) =>{
 
               if(err){
-                console.log("je passe ici")
                 return res.status(400).json({message : err.message})
               }else{
                 db.query(`UPDATE Post SET likes= ? WHERE id_post= ?`,[req.body.nblikes ,req.body.id_post], (err, result) =>{
                   
                   if(err){
-                    console.log("je passe ici 2")
                     return res.status(400).json({message : err.message})
                   }else{
                     return res.status(200).json({message : "Vous n'aimez plus le post"})
-                  }
-                 
-
+                  }               
                 });
               }
-
             })
           }else{
             return res.status(200).json({message : "Tu aimes déjà"})
           }
-
         });
-        //avant verifier si ton user est bien dans la table like SELECT
-          // ici faire le delete dans la table like DELETE
-          //decrementer la valeur like dans la table post UPDATE
         
         break;
-       
-        case 1:
+         // message liké //
+        case 1:         
           db.query(`SELECT * FROM groupomania.like WHERE id_user = ? AND id_post= ?`,[req.body.id_user, req.body.id_post], (err, result) =>{
             if(err){
               
               return res.status(400).json({message : err.message})
             }
-            if(result.lenght<0){
+            if(result){
+              return res.status(200).json({message : "Tu aimes déjà"})
+            }else{
               db.query(`INSERT FROM groupomania.like WHERE id_user= ? AND id_post= ?`,[req.body.id_user, req.body.id_post], (err, result) =>{
-  
-                if(err){
+                  if(err){
                   return res.status(400).json({message : err.message})
                 }else{
                   db.query(`UPDATE Post SET likes= ? WHERE id_post= ?`,[req.body.nblikes ,req.body.id_post], (err, result) =>{
-                    
-                    if(err){
+                                        if(err){
                       return res.status(400).json({message : err.message})
                     }else{
                       return res.status(200).json({message : "Vous aimez le post"})
-                    }
-                   
-  
+                    }                   
                   });
                 }
   
               })
-            }else{
-              return res.status(200).json({message : "Tu aimes déjà"})
-            }
-  
+            }  
           });
-        //avant verifier si ton user est bien dans la table like SELECT
-          // si ton utilisaateur n'est dans la table like ici faire l'ajout dans la table like INSERT
-          // incrementer la valeur like dans la table post UPDATE
-        break;
+                break;
       }
    }
